507e31d565db4a8f94ee41faaf649c02e7b58079
Patrick Hollweck <patrick-hollweck@gmx.de>
2018-05-27 16:06:55 +0200

    Added deploy script and upadated deps

---
+++ scripts/deploy.js
@@ -0,0 +1,32 @@
+// @ts-check
+
+const fs = require("fs-extra");
+const shell = require("shelljs");
+const consola = require("consola");
+
+consola.warn('Make sure you are in the projects ROOT!');
+consola.start("Cleaning previous build files!");
+
+fs.removeSync("./dist");
+
+consola.success("Removed previous build files");
+consola.start("Compiling the project...");
+
+const tscOut = shell.exec("tsc").stdout;
+if (tscOut.includes("error")) {
+	consola.error("TSC Output contained a error!");
+	process.exit(1);
+}
+
+consola.success("Project compiled!");
+consola.start("Copy package.json to output files...");
+
+fs.copyFileSync("./package.json", "./dist/package.json");
+
+consola.success("Copied package.json to output");
+consola.start("Publishing to npm...");
+
+shell.cd("./dist");
+shell.exec("npm publish");
+
+consola.success("Published!");
