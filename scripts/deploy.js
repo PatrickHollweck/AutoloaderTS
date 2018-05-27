b1eb189f064b4850f236710be8632b16546fb228
Patrick Hollweck <patrick-hollweck@gmx.de>
2018-05-27 17:03:38 +0200

    Move readme to dist in deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,7 @@ const fs = require("fs-extra");
 const shell = require("shelljs");
 const consola = require("consola");
 
-consola.warn('Make sure you are in the projects ROOT!');
+consola.warn('Make sure you are in the projects ROOT! And you did update the version field in the package.json');
 consola.start("Cleaning previous build files!");
 
 fs.removeSync("./dist");
@@ -22,6 +22,7 @@ consola.success("Project compiled!");
 consola.start("Copy package.json to output files...");
 
 fs.copyFileSync("./package.json", "./dist/package.json");
+fs.copyFileSync("./README.md", "./dist/README.md");
 
 consola.success("Copied package.json to output");
 consola.start("Publishing to npm...");
